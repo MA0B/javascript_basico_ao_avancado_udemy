22345002ee5f3d4deab0939264f77cccab13f53e
MA0B <morrice016@outlook.com>
2025-04-26 11:31:57 -0300

    up

---
+++ Estruturas de programação/1_variaveis/script.js
@@ -0,0 +1,16 @@
+let nome = "Maurício";
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5
+
+console.log(laranjas = laranjas);
+
+nome = "João";
+console.log(nome);
+
+laranja = 83284;
+
+console.log(laranja);
+laranja = "laranja";
+console.log(laranja)

9e12407f92dfc52e5c0cff1c8f5a136807c52c4c
MA0B <morrice016@outlook.com>
2025-04-28 11:20:56 -0300

    update

---
+++ Estruturas de programação/8_consoleLog/script.js
@@ -0,0 +1,8 @@
+let idade = 25;
+let nome = "Maurício";
+
+console.log(idade);
+console.log(nome);
+
+
+console.log(`O meu nome é ${nome} e tenho ${idade} anos`);
---
+++ Estruturas de programação/9_if_else_elseif/script.js
@@ -0,0 +1,11 @@
+let idade = 19;
+
+if(idade > 18) {
+    console.log('pode entrar');
+}
+
+if (idade > 25) {
+    console.log('A idade é maior que 25');
+}else if (idade < 25) {
+        console.log('A idade é menor que 25');
+}
